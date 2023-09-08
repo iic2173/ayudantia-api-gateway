7c789b5fe1044a2d02ec19a3c89e210487fe4898
Gerardo Crot Rojas <gicrot@uc.cl>
2023-09-07 23:13:24 -0300

    fix: cors methods for options

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require('@koa/cors');
 
 const corsOptions = {
   origin: '*',
-  allowHeaders: ['Auth'],
+  allowHeaders: ['Access-Control-Allow-Headers', 'Origin','Accept', 'X-Requested-With', 'Content-Type', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Auth'],
+  allowMethods: ['GET','HEAD','OPTIONS', 'PUT','POST','DELETE','PATCH'],
 };
 
 const app = new Koa();
