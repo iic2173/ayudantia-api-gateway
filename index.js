0c78939142442dae7274a4ed605fabb9002ed511
VicenteLavagnino <vicente.lavagnino@uc.cl>
2025-04-10 18:32:35 -0400

    con cors

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const Router = require('koa-router');
 const cors = require('@koa/cors');
 
 const corsOptions = {
-  // origin: '*',
-  // allowHeaders: ['Access-Control-Allow-Headers', 'Origin','Accept', 'X-Requested-With', 'Content-Type', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Auth'],
-  // allowMethods: ['GET','HEAD','OPTIONS', 'PUT','POST','DELETE','PATCH'],
+  origin: '*',
+  allowHeaders: ['Access-Control-Allow-Headers', 'Origin','Accept', 'X-Requested-With', 'Content-Type', 'Access-Control-Request-Method', 'Access-Control-Request-Headers', 'Auth'],
+  allowMethods: ['GET','HEAD','OPTIONS', 'PUT','POST','DELETE','PATCH'],
 };
 
 const app = new Koa();
